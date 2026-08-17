39cf092d9b6ff8756d5e4817d152595057d4a074
sjsiwat <sj.siwat@gmail.com>
2026-08-17 21:25:09 +0700

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="flex flex-col text-2xl text-slate-300 font-bold text-center py-6 animate-bounce">
+    <div className="flex flex-wrap flex-col text-2xl text-slate-300 font-bold text-center py-6 animate-bounce">
       <h1 className="hover:text-red-400 font-extrabold">
         This is How responsive website work ?
       </h1>
